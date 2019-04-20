b3d792be1dc88ec4651a352aaeef648426c9b926
TioaTyan <xiaozhidiguo@gmail.com>
2019-04-20 14:50:05 +0800

    购物车页面：样式调整&交互逻辑调整

--- B2C_NetShop/Goods/cart.aspx.cs
+++ B2C_NetShop/Goods/cart.aspx.cs
@@ -9,145 +9,147 @@ using B2C_NetShop.App_Start;
 
 namespace B2C_NetShop.Goods
 {
-    public partial class cart : System.Web.UI.Page
-    {
-        pageload load = new pageload();
-        Database operate = new Database();
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            HyperLink hl1 = (HyperLink)(Master.FindControl("HyperLink1"));//用户个人资料
-            HyperLink hl2 = (HyperLink)(Master.FindControl("HyperLink2"));//后台
-            HyperLink hl3 = (HyperLink)(Master.FindControl("HyperLink3"));//注册、注销
-            String uid = Convert.ToString(Session["uid"]);
-            String nickname = Convert.ToString(Session["nickname"]);
-            Label_UID1.Text = nickname;
-            int status = Convert.ToInt32(Session["Status"]);
-            load.HyperLinkBind(hl1, hl2, hl3, uid, status);
-            if(!IsPostBack)
-            {
-                BindCartList();
-            }   
-        }
+	public partial class cart : System.Web.UI.Page
+	{
+		pageload load = new pageload();
+		Database operate = new Database();
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			HyperLink hl1 = (HyperLink)(Master.FindControl("HyperLink1"));//用户个人资料
+			HyperLink hl2 = (HyperLink)(Master.FindControl("HyperLink2"));//后台
+			HyperLink hl3 = (HyperLink)(Master.FindControl("HyperLink3"));//注册、注销
+			String uid = Convert.ToString(Session["uid"]);
+			String nickname = Convert.ToString(Session["nickname"]);
+			Label_UID1.Text = nickname;
+			int status = Convert.ToInt32(Session["Status"]);
+			load.HyperLinkBind(hl1, hl2, hl3, uid, status);
+			if (!IsPostBack)
+			{
+				BindCartList();
+			}
+		}
 
-        public void BindCartList()
-        {
-            if (Session["ShopCart"] == null)
-            {//购物车不存在
-                Response.Redirect("~/Account/Login.aspx");
-            }
-            else
-            {//购物车存在
-                Hashtable hashCart = (Hashtable)Session["ShopCart"];
-                if (hashCart.Count == 0)
-                {//没有购物
-                    
-                }
-                else
-                {//设置数据源
-                    DataTable dtTable = new DataTable();
-                    DataColumn column1 = new DataColumn("No");
-                    DataColumn column2 = new DataColumn("BookID");
-                    DataColumn column3 = new DataColumn("BookName");
-                    DataColumn column4 = new DataColumn("Num");
-                    DataColumn column5 = new DataColumn("Price");
-                    DataColumn column6 = new DataColumn("totalPrice");
-                    dtTable.Columns.Add(column1);
-                    dtTable.Columns.Add(column2);
-                    dtTable.Columns.Add(column3);
-                    dtTable.Columns.Add(column4);
-                    dtTable.Columns.Add(column5);
-                    dtTable.Columns.Add(column6);
-                    DataRow row;
-                    foreach (object key in hashCart.Keys)
-                    {
-                        row = dtTable.NewRow();
-                        String bookid = key.ToString();
-                        row["BookID"] = bookid;
-                        row["Num"] = hashCart[key].ToString();
-                        dtTable.Rows.Add(row);
-                    }
-                    int i = 1;
-                    float price;
-                    int count;
-                    float totalPrice = 0;
-                    foreach (DataRow drRow in dtTable.Rows)
-                    {//遍历，获取图书名称，单价
-                     //新建sql查询
-                        //SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["B2C_DemoConnectionString"].ConnectionString.ToString());
-                        String BookID = drRow["BookID"].ToString();
-                        String sql = "select BookName,HotPrice from Goods_Info where BookID=@bookid";
-                        SqlParameter[] parameters = {
-                            new SqlParameter("@bookid",BookID)
-                        };
-                        //try
-                        //{
-                        //  conn.Open();
-                        //    SqlDataAdapter sda = new SqlDataAdapter(sql, conn);
-                            DataSet ds = operate.GetTable(sql, parameters);
-                            //填充
-                            drRow["No"] = i;
-                            drRow["BookName"] = ds.Tables[0].Rows[0][0].ToString(); //读取名称
-                            drRow["price"] = (ds.Tables[0].Rows[0][1].ToString());  //读取单价
-                            price = float.Parse(ds.Tables[0].Rows[0][1].ToString());
-                            count = Int32.Parse(drRow["Num"].ToString());
-                            drRow["totalPrice"] = price * count;
-                            totalPrice += price * count;
-                            i++;
-                        //}
-                        //catch (Exception ex)
-                        //{
-                        //    Response.Write(ex.Message);
-                        //}
-                        //finally
-                        //{
-                        //    conn.Close();
-                        //}
-                        labTotalPrice.Text = "￥" + totalPrice.ToString();
-                        gvShopCart.DataSource = dtTable.DefaultView;
-                        gvShopCart.DataKeyNames = new string[] { "BookID" };
-                        gvShopCart.DataBind();
-                    }
-                }
-            }
-        }
+		public void BindCartList()
+		{
+			if (Session["ShopCart"] == null)
+			{//购物车不存在
+				Response.Redirect("~/Account/Login.aspx");
+			}
+			else
+			{//购物车存在
+				Hashtable hashCart = (Hashtable)Session["ShopCart"];
+				if (hashCart.Count == 0)
+				{//没有购物
+					cart_feature_ifNotEmpty.Style["display"] = "none";
+				}
+				else
+				{
+					cart_feature_ifEmpty.Style["display"] = "none";
+					//设置数据源
+					DataTable dtTable = new DataTable();
+					DataColumn column1 = new DataColumn("No");
+					DataColumn column2 = new DataColumn("BookID");
+					DataColumn column3 = new DataColumn("BookName");
+					DataColumn column4 = new DataColumn("Num");
+					DataColumn column5 = new DataColumn("Price");
+					DataColumn column6 = new DataColumn("totalPrice");
+					dtTable.Columns.Add(column1);
+					dtTable.Columns.Add(column2);
+					dtTable.Columns.Add(column3);
+					dtTable.Columns.Add(column4);
+					dtTable.Columns.Add(column5);
+					dtTable.Columns.Add(column6);
+					DataRow row;
+					foreach (object key in hashCart.Keys)
+					{
+						row = dtTable.NewRow();
+						String bookid = key.ToString();
+						row["BookID"] = bookid;
+						row["Num"] = hashCart[key].ToString();
+						dtTable.Rows.Add(row);
+					}
+					int i = 1;
+					float price;
+					int count;
+					float totalPrice = 0;
+					foreach (DataRow drRow in dtTable.Rows)
+					{//遍历，获取图书名称，单价
+					 //新建sql查询
+					 //SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["B2C_DemoConnectionString"].ConnectionString.ToString());
+						String BookID = drRow["BookID"].ToString();
+						String sql = "select BookName,HotPrice from Goods_Info where BookID=@bookid";
+						SqlParameter[] parameters = {
+							new SqlParameter("@bookid",BookID)
+						};
+						//try
+						//{
+						//  conn.Open();
+						//    SqlDataAdapter sda = new SqlDataAdapter(sql, conn);
+						DataSet ds = operate.GetTable(sql, parameters);
+						//填充
+						drRow["No"] = i;
+						drRow["BookName"] = ds.Tables[0].Rows[0][0].ToString(); //读取名称
+						drRow["price"] = (ds.Tables[0].Rows[0][1].ToString());  //读取单价
+						price = float.Parse(ds.Tables[0].Rows[0][1].ToString());
+						count = Int32.Parse(drRow["Num"].ToString());
+						drRow["totalPrice"] = price * count;
+						totalPrice += price * count;
+						i++;
+						//}
+						//catch (Exception ex)
+						//{
+						//    Response.Write(ex.Message);
+						//}
+						//finally
+						//{
+						//    conn.Close();
+						//}
+						labTotalPrice.Text = "￥" + totalPrice.ToString();
+						gvShopCart.DataSource = dtTable.DefaultView;
+						gvShopCart.DataKeyNames = new string[] { "BookID" };
+						gvShopCart.DataBind();
+					}
+				}
+			}
+		}
 
-        protected void lnkbtnUpdate_Click(object sender, EventArgs e)
-        {
-            Hashtable hashCart = (Hashtable)Session["ShopCart"];
-            foreach (GridViewRow gvr in this.gvShopCart.Rows)
-            {
-                TextBox otb = (TextBox)gvr.FindControl("txtNum");
-                int count = Int32.Parse(otb.Text);
-                string BookID = gvr.Cells[1].Text;
-                hashCart[BookID] = count;
-            }
-            Session["ShopCart"] = hashCart;
-            BindCartList();
-        }
+		protected void btn_cart_update_Click(object sender, EventArgs e)
+		{
+			Hashtable hashCart = (Hashtable)Session["ShopCart"];
+			foreach (GridViewRow gvr in this.gvShopCart.Rows)
+			{
+				TextBox otb = (TextBox)gvr.FindControl("txtNum");
+				int count = Int32.Parse(otb.Text);
+				string BookID = gvr.Cells[1].Text;
+				hashCart[BookID] = count;
+			}
+			Session["ShopCart"] = hashCart;
+			BindCartList();
+		}
 
-        protected void lnkbtnDelete_Command(object sender, CommandEventArgs e)
-        {
-            Hashtable hashCart = (Hashtable)Session["ShopCart"];
-            String removeid = "id=" + e.CommandArgument.ToString();
-            hashCart.Remove(removeid);
-            Session["ShopCart"] = hashCart;
-            Response.Redirect("cart.aspx");
-        }
+		protected void btn_cart_clear_Click(object sender, EventArgs e)
+		{
+			Session["ShopCart"] = null;
+			Hashtable hashCart = new Hashtable();
+			Session["ShopCart"] = hashCart;
+			Response.Redirect("~/Goods/cart.aspx");
+		}
 
-        protected void lnkbtnClear_Click(object sender, EventArgs e)
-        {
-            Session["ShopCart"] = null;
-            Hashtable hashCart = new Hashtable();
-            Session["ShopCart"] = hashCart;
-            Response.Redirect("~/Goods/cart.aspx");
-        }
+		protected void btn_cart_continue_Click(object sender, EventArgs e)
+		{
+			Response.Redirect("~/Default.aspx");
+		}
 
-        protected void lnkbtnContinue_Click(object sender, EventArgs e)
-        {
-            Response.Redirect("~/Default.aspx");
-        }
+		protected void lnkbtnDelete_Command(object sender, CommandEventArgs e)
+		{
+			Hashtable hashCart = (Hashtable)Session["ShopCart"];
+			String removeid = "id=" + e.CommandArgument.ToString();
+			hashCart.Remove(removeid);
+			Session["ShopCart"] = hashCart;
+			Response.Redirect("cart.aspx");
+		}
 
-        /*
+		/*
         protected void gvShopCart_PageIndexChanging(object sender, GridViewPageEventArgs e)
         {
             gvShopCart.PageIndex = e.NewPageIndex;
@@ -155,9 +157,9 @@ namespace B2C_NetShop.Goods
         }
         */
 
-        protected void ImageButton1_Click(object sender, ImageClickEventArgs e)
-        {
-            Response.Redirect("~/Order/getOrderInfo.aspx");
-        }
-    }
+		protected void btn_pay_Click(object sender, EventArgs e)
+		{
+			Response.Redirect("~/Order/getOrderInfo.aspx");
+		}
+	}
 }
--- B2C_NetShop/Goods/cart.aspx.designer.cs
+++ B2C_NetShop/Goods/cart.aspx.designer.cs
@@ -13,15 +13,6 @@ namespace B2C_NetShop.Goods {
     public partial class cart {
         
         /// <summary>
-        /// main_content 控件。
-        /// </summary>
-        /// <remarks>
-        /// 自动生成的字段。
-        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlGenericControl main_content;
-        
-        /// <summary>
         /// welcome 控件。
         /// </summary>
         /// <remarks>
@@ -49,15 +40,6 @@ namespace B2C_NetShop.Goods {
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl head_nav;
         
         /// <summary>
-        /// Gcart 控件。
-        /// </summary>
-        /// <remarks>
-        /// 自动生成的字段。
-        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlGenericControl Gcart;
-        
-        /// <summary>
         /// gvShopCart 控件。
         /// </summary>
         /// <remarks>
@@ -67,31 +49,49 @@ namespace B2C_NetShop.Goods {
         protected global::System.Web.UI.WebControls.GridView gvShopCart;
         
         /// <summary>
-        /// lnkbtnUpdate 控件。
+        /// cart_feature_ifEmpty 控件。
         /// </summary>
         /// <remarks>
         /// 自动生成的字段。
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton lnkbtnUpdate;
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl cart_feature_ifEmpty;
         
         /// <summary>
-        /// lnkbtnClear 控件。
+        /// cart_feature_ifNotEmpty 控件。
         /// </summary>
         /// <remarks>
         /// 自动生成的字段。
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton lnkbtnClear;
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl cart_feature_ifNotEmpty;
         
         /// <summary>
-        /// lnkbtnContinue 控件。
+        /// btn_cart_update 控件。
         /// </summary>
         /// <remarks>
         /// 自动生成的字段。
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton lnkbtnContinue;
+        protected global::System.Web.UI.WebControls.Button btn_cart_update;
+        
+        /// <summary>
+        /// btn_cart_clear 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_cart_clear;
+        
+        /// <summary>
+        /// btn_cart_continue 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_cart_continue;
         
         /// <summary>
         /// labTotalPrice 控件。
@@ -103,12 +103,12 @@ namespace B2C_NetShop.Goods {
         protected global::System.Web.UI.WebControls.Label labTotalPrice;
         
         /// <summary>
-        /// ImageButton1 控件。
+        /// btn_pay 控件。
         /// </summary>
         /// <remarks>
         /// 自动生成的字段。
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ImageButton ImageButton1;
+        protected global::System.Web.UI.WebControls.Button btn_pay;
     }
 }
