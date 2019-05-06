ac3360f7f4b09a8d5c9e4dded3ae7dca1a92700d
TioaTyan <xiaozhidiguo@gmail.com>
2019-05-06 23:29:22 +0800

    头像修改：样式调整

--- B2C_NetShop/User/Info.aspx.designer.cs
+++ B2C_NetShop/User/Info.aspx.designer.cs
@@ -274,15 +274,6 @@ namespace B2C_NetShop.User {
         protected global::System.Web.UI.WebControls.View View_UserImage;
         
         /// <summary>
-        /// Image2 控件。
-        /// </summary>
-        /// <remarks>
-        /// 自动生成的字段。
-        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Image Image2;
-        
-        /// <summary>
         /// FileUpload1 控件。
         /// </summary>
         /// <remarks>
@@ -301,13 +292,13 @@ namespace B2C_NetShop.User {
         protected global::System.Web.UI.WebControls.Button Btnupload;
         
         /// <summary>
-        /// btndelete 控件。
+        /// Image2 控件。
         /// </summary>
         /// <remarks>
         /// 自动生成的字段。
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btndelete;
+        protected global::System.Web.UI.WebControls.Image Image2;
         
         /// <summary>
         /// Button1 控件。
